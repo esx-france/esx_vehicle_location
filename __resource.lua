b4c246851dd55319d08e27bc457b36e7873a72e4
Kevin <34973659+keketiger@users.noreply.github.com>
2019-12-15 23:15:04 +0100

    upgrade to version 0.0.2

--- __resource.lua
+++ __resource.lua
@@ -1,9 +1,7 @@
 resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
-
-name 'ESX Vehicle Location'
 description 'This script allows you to rent vehicles to the newcomer for example at the airport of your city.'
 website 'https://github.com/ESX-FRANCE/esx_vehicle_location'
-version '0.0.1'
+version '0.0.2'
 author 'keketiger'
 
 server_scripts {
