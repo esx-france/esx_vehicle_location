a65e44fee9cbb6108345c1a40b23154c7f4782b2
Kevin <34973659+keketiger@users.noreply.github.com>
2019-12-15 23:10:03 +0100

    upgrade to version 0.0.2

--- config.lua
+++ config.lua
@@ -7,27 +7,28 @@ Config.MarkerColor  = { r = 255, g = 255, b = 255 }
 
 Config.Locale = 'fr'
 
-Config.Blip = {
-    {
-        Coords = vector3(-1035.2, -2732.8, 20.1),
-        Sprite = 171,
-        Scale = 1.0,
-        Color = 5
-    }
-}
+Config.Locations = {
 
-Config.Vehicles = {
-    Spawner = {
-        vector3(-1035.2, -2732.8, 20.1)
+    Blip = {
+        Coords  = vector3(-1035.2, -2732.8, 20.1),
+        Sprite  = 171,
+        Display = 4,
+        Scale   = 1.0,
+        Color   = 5
     },
-    SpawnPoints = {
-        { coords = vector3(-1033.6, -2730.7, 19.5), heading = 239.24, radius = 3.0 },
-        { coords = vector3(-1027.4, -2734.5, 19.5), heading = 238.72, radius = 3.0 },
-        { coords = vector3(-1038.7, -2727.7, 19.5), heading = 238.92, radius = 3.0 }
-    }
-}
 
-Config.Location = {
-    { model = 'kalahari', label = 'Canis Kalahari', price = 100 },
-    { model = 'Faggio2', label = 'Scooter', price = 50 }
+    Vehicles = {
+        {
+            Spawner = vector3(-1035.2, -2732.8, 20.1),
+            SpawnPoints = {
+                { coords = vector3(-1033.6, -2730.7, 19.5), heading = 239.24, radius = 3.0 },
+                { coords = vector3(-1027.4, -2734.5, 19.5), heading = 238.72, radius = 3.0 },
+                { coords = vector3(-1038.7, -2727.7, 19.5), heading = 238.92, radius = 3.0 }
+            },
+            Models = {
+                { model = 'kalahari', label = 'Canis Kalahari', price = 100 },
+                { model = 'Faggio2', label = 'Scooter', price = 50 }
+            }
+        }
+    }
 }
